2026aaa25e64af63b386a8196ee2ca6e8a6a8e5f
Ant1venom <83695767+Ant1venom@users.noreply.github.com>
2022-03-28 17:12:04 +0200

    login api, and the server is now possibly a teapot

--- backend/projekt_backend/login/views.py
+++ backend/projekt_backend/login/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from db_api.models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 
 @csrf_exempt
+@require_http_methods('POST')
 def login(request):
     try:
         posted_username = request.POST['username']
         posted_password = request.POST['password']
     except KeyError:
-        return HttpResponse('Username or password not provided', status=418)
+        return HttpResponse(reason='Username or password not provided', status=418)
     try:
         login_user = User.objects.all().get(username=posted_username, password=posted_password)
     except User.DoesNotExist:
-        return HttpResponse('Username or password invalid', status=418)
+        return HttpResponse(reason='Username or password is invalid', status=418)
     return JsonResponse({'data': {'role': login_user.role}})
 # Create your views here.
