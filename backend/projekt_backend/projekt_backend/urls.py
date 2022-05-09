30b350470231280671b5d912e08596113a4ab2cb
Ant1venom <83695767+Ant1venom@users.noreply.github.com>
2022-05-09 16:30:47 +0200

    view site login redirect

--- backend/projekt_backend/projekt_backend/urls.py
+++ backend/projekt_backend/projekt_backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_url = 'https://localhost:3000'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('db_api.urls')),
